4100edb7d9967a6ad859e52fb0b6d2e6750a4a27
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-06-23 13:48:42 +0600

    trending component added

---
+++ src/Components/topTrending/TopTrending.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopTrending = () => {
+    return (
+        <div>
+            <h5>this is top trending page</h5>
+        </div>
+    );
+};
+
+export default TopTrending;
